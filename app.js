7f4f86d48c1024dac82b42067ea8b10522b80af5
Dee-daniel <akinboladare1960@gmail.com>
2023-02-02 03:45:03 -0800

    added more functionality to controller

--- app.js
+++ app.js
@@ -6,17 +6,19 @@ const app = express();
 const PORT = 3900
 mongoose.set('strictQuery', true)
 const taskRouter = require("./routes/taskRouter")
+const notFound = require ("./middleware/notFoundRoute")
+const errorHandler = require("./middleware/errorHandler")
 
 //middleware
 app.use(express.json());
 
 //routes
 app.use("/api/v1/tasks", taskRouter);
+app.use(errorHandler);
 
-//error
-app.use((req, res) => {
-    res.status(404).json({msg: '404 Not Found'});
-})
+//error route
+app.use(notFound)
+
 
 //DBconnections
 
